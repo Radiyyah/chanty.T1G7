755e0e180e02bc93dcc0c6872fcf9a2b414094f8
Adekorede <adekoredeaderibigbe@gmail.com>
2022-08-29 22:17:03 +0100

    worked on the nav bar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// function myFunction() {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+// }
+
+// var closebtns = document.getElementsByClassName("close");
+// var i;
+
+// for (i = 0; i < closebtns.length; i++) {
+//   closebtns[i].addEventListener("click", function() {
+//     this.parentElement.style.display = 'none';
+//   });
+// }
