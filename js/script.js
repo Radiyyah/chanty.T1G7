3c5d06756c83cf328907e7a92827ac88f41b6481
Zacchaeus <raintech23@gmail.com>
2022-08-31 14:21:49 +0100

    added some code

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let i = 0; //start point
+let container = [];
+let time = 3000;
+//container list
+container[0] = document.getElementsByClassName('section1');
+container[1] = document.getElementsByClassName('section2');
+container[2] = document.getElementsByClassName('section3');
+container[3] = document.getElementsByClassName('section4');
+
+//Function to change the container
+function changeContainer(){
+    
+}
+changeContainer();
+
+// document.slide.src = container[i];
+
+//     if(i < container.length -1){
+//         i++;
+//     }else{
+//         i = 0;
+//     }
+
+//     setTimeout("changeContainer()", time);
