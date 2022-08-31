f9c3a7f4c7303cc02d598046654d597f4bb7fe28
Samxord <samxord245@gmail.comm>
2022-08-31 21:04:49 +0530

    Edited navbar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,58 @@
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
+$(document).ready(function(){
+  $('#icon').click(function(){
+      $('ul').toggleClass('show');
+  });
+});
+
+let dropdown = document.querySelector('.nav-3');
+dropdown.onclick = function(){
+  dropdown.classList.toggleClass('active');
+} 
+const toggleBtn = document.querySelector('#close-icon');
+const navlist = document.querySelector('menu');
+
+toggleBtn.addEventListener('click', () =>{
+if(navlist.style.display === 'none') {
+  navlist.style.display ='block';
+} else {
+  navlist.style.display ='none';
+}
+});
+// const list = document.querySelector("#menu ul");
+// const forms = document.forms;
+
+// // delete movie
+// list.addEventListener("click", (e) => {
+//   if (e.target.className == "close-icon") {
+//     const li = e.target.parentElement;
+//     li.parentNode.removeChild(li);
+//   }
+// });
+  
+// window.addEventListener('scroll', reveal);
+
+// function reveal(){
+//   let reveals = document.querySelectorAll('.scroll-nav');
+
+//   for(let i = 0; i < reveals.length; i++){
+
+//     let windowheight = window.innerHeight;
+//     let revealtop = reveals[i].getBoundingClientRect().top;
+//     let revealpoint = 150;
+
+//     if(revealtop < windowheight - revealtop){
+//       reveals[i].classList.add('active');
 //     }
-// }
-
-// var closebtns = document.getElementsByClassName("close");
-// var i;
-
-// for (i = 0; i < closebtns.length; i++) {
-//   closebtns[i].addEventListener("click", function() {
-//     this.parentElement.style.display = 'none';
-//   });
+//     else{
+//       reveals[i].classList.remove('active');
+//     }
+//   }
 // }
