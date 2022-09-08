5403674c88c3ecd0f42d8ce4baefe92403826849
Adekorede <adekoredeaderibigbe@gmail.com>
2022-09-08 22:44:52 +0100

    made corrections

--- js/script.js
+++ js/script.js
@@ -32,18 +32,10 @@ if(i = container){
 //     setTimeout("changeContainer()", time);
 
 
-// for (i = 0; i < closebtns.length; i++) {
-//   closebtns[i].addEventListener("click", function() {
-//     this.parentElement.style.display = 'none';
-//   });
-// }
-// $(document).ready(function(){
-//   $('#icon').click(function(){
-//       $('ul').toggleClass('show');
-//   });
-// });
 
-// icon-section
+// HEADER JS STARTS
+// NAV BAR STARTS
+// icon-section BY SAMUEL
 const icon = document.getElementById('icon');
 
 const menu = document.getElementById('menu');
@@ -66,47 +58,8 @@ let dropdown = document.querySelector('.nav-3');
 dropdown.onclick = function(){
   dropdown.classList.toggleClass('active');
 } 
-// const toggleBtn = document.querySelector('#close-icon');
-// const navlist = document.querySelector('menu');
-
-// toggleBtn.addEventListener('click', () =>{
-// if(navlist.style.display === 'none') {
-//   navlist.style.display ='block';
-// } else {
-//   navlist.style.display ='none';
-// }
-// });
-// const list = document.querySelector("#menu ul");
-// const forms = document.forms;
-
-// // delete movie
-// list.addEventListener("click", (e) => {
-//   if (e.target.className == "close-icon") {
-//     const li = e.target.parentElement;
-//     li.parentNode.removeChild(li);
-//   }
-// });
-  
-// window.addEventListener('scroll', reveal);
-
-// function reveal(){
-//   let reveals = document.querySelectorAll('.scroll-nav');
-
-//   for(let i = 0; i < reveals.length; i++){
-
-//     let windowheight = window.innerHeight;
-//     let revealtop = reveals[i].getBoundingClientRect().top;
-//     let revealpoint = 150;
-
-//     if(revealtop < windowheight - revealtop){
-//       reveals[i].classList.add('active');
-//     }
-//     else{
-//       reveals[i].classList.remove('active');
-//     }
-//   }
-// }
-
+// NAV BAR ENDS
+// HEADER JS ENDS
 // main starts
 
 // pricing section starts
